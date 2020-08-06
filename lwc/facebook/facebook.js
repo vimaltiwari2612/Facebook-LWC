03adce169a96cf71da2f238ccf9f8fb0240643bd
Vimal Tiwari <vimaltiwari2612@gmail.com>
2020-08-06 14:34:20 +0530

    Update facebook.js

--- lwc/facebook/facebook.js
+++ lwc/facebook/facebook.js
@@ -1,12 +1,19 @@
 import { LightningElement } from 'lwc'; 
+import { track } from 'lwc'; 
 import getFeeds from "@salesforce/apex/RequestProcessor.getFeeds";
 export default class LwcMakeCallout extends LightningElement {
-    receivedMessage = [];
+    @track receivedMessage = [];
     hasRendered = false;
-    
+   
     getFeeds() {
         getFeeds().then(response=>{
             this.receivedMessage = JSON.parse(response);
+            for(var i = 0; i < this.receivedMessage.feed.length; i++){
+                this.receivedMessage.feed[i].like = 0;
+                this.receivedMessage.feed[i].comment = 0;
+                this.receivedMessage.feed[i].share = 0;
+            }
+            console.log(this.receivedMessage);
         }).catch(error=>{
             console.error(error);
         })
@@ -17,5 +24,19 @@ export default class LwcMakeCallout extends LightningElement {
             this.getFeeds();
             this.hasRendered = true;
         }
+    }
+
+    handleClick(event) {
+       
+        var id = parseInt(event.target.name);
+        var label = event.target.label;
+        
+        if(label === "Like")
+            this.receivedMessage.feed[id-1].like++;
+        if(label === "Comment")
+            this.receivedMessage.feed[id-1].comment++;
+        if(label === "Share")
+            this.receivedMessage.feed[id-1].share++;
+        
     }
 }
